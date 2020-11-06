19e2a5b7c702ce14ad8e776fb9971c7eb0cb7944
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2020-11-06 07:24:39 -0300

    Bringing logs to the foreground

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,18 @@ setgid = nogroup
 pid = /sck/stunnel.pid
 debug = 7
 delay = yes
+output = /var/log/stunnel4/stunnel.log
 [client]
   client = yes
 EOT
 echo "  accept = 127.0.0.1:${LOCAL_PORT}"  >> /etc/stunnel/stunnel.conf
 echo "  connect = ${HOST}:${REMOTE_PORT}"  >> /etc/stunnel/stunnel.conf
 
+mkdir -p /var/log/stunnel4
+touch /var/log/stunnel4/stunnel.log
+chown -R nobody:nogroup /var/log/stunnel4
+
 mkdir /sck
 chown -R nobody:nogroup /sck
 /usr/bin/stunnel /etc/stunnel/stunnel.conf
-sleep infinity
+tail -f /var/log/stunnel4/stunnel.log
